6da86427db049c6a81304d1577a04b0f7a06ba80
Matt Stump <mstump@sourceninja.com>
2012-02-06 21:03:37 -0800

    added functions for counting all rows, and counting unique groupid/artifiact ID combinations

--- src/test/tuples.clj
+++ src/test/tuples.clj
@@ -1,6 +1,6 @@
 (ns test.tuples
   (:use cascalog.api)
-  (:require [cascalog [vars :as v]])
+  (:require [cascalog [vars :as v] [ops :as o]])
   (:gen-class))
 
 (def artifact-keys
@@ -11,21 +11,17 @@
    :bundle_description :bundle_name :bundle_license :bundle_docurl
    :bundle_importpkg])
 
-(def artifact-fields
-  ["?groupid" "?artifactid" "?version" "?size" "!sha1" "!md5" "!path"
-   "!prefix" "?javadoc?" "!source?" "?signature?" "!classnames"
-   "?lastmodified" "!packaging" "!repository" "!remoteurl"
-   "!description" "!fname" "!fextension" "!bundle_version"
-   "!bundle_symname" "!bundle_exportpkg" "!bundle_exportsrv"
-   "!bundle_description" "!bundle_name" "!bundle_license"
-   "!bundle_docurl" "!bundle_importpkg"])
-
 (defn create-artifact-tuple
+  "convert the input map to a vector with the values in the order
+   supplied by artifact-keys"
   [input]
   (let [artifact (read-string input)]
     (into [] (map (partial get artifact) artifact-keys))))
 
-(defn textline-parsed [dir]
+(defn textline-parsed
+  "take a line from the input tap which is a hash serialized as an
+   s-expression, and turn it into a tuple with named fields"
+  [dir]
   (let [source (hfs-textline dir)]
     (<- [?groupid ?artifactid ?version ?size !sha1 !md5 !path
          !prefix ?javadoc? !source? ?signature? !classnames
@@ -42,15 +38,44 @@
                                !bundle_symname !bundle_exportpkg !bundle_exportsrv
                                !bundle_description !bundle_name !bundle_license
                                !bundle_docurl !bundle_importpkg))))
-;        (name-vars ?line artifact-fields))))
 
-(defn query
+(defn query-uniq-group-ids
+  "return distinct occurances of ?groupid ?version and !fextension"
   [output-tap input-path]
   (?<- output-tap
-       [?groupid ?version !fextension ?lastmodified]
+       [?groupid ?version !fextension]
        ((select-fields (textline-parsed input-path)
-                       ["?groupid" "?version" "!fextension" "?lastmodified"])
-        ?groupid ?version !fextension ?lastmodified)))
+                       ["?groupid" "?version" "!fextension"])
+        ?groupid ?version !fextension )
+       (:distinct true)))
 
-(defn -main [input-path]
-  (query (stdout) input-path))
+(defn max-lastchanged
+  "return max lastchanged for each group ?groupid ?version and !fextension"
+  [input]
+  (o/first-n input
+             1
+             :sort "?lastmodified"
+             :reverse true))
+
+(defn get-package-identity
+  [groupid artifactid]
+  (str groupid "/" artifactid))
+
+(defn get-identity-count
+  [input-path]
+  (?<- (stdout)
+       [?identity ?count]
+       ((select-fields (textline-parsed input-path) ["?groupid" "?artifactid"]) ?groupid ?artifactid)
+       (get-package-identity ?groupid ?artifactid :> ?identity)
+       (o/count ?count)))
+
+(defn get-row-count
+  [input-path]
+  (?<- (stdout)
+       [?count]
+       ((select-fields (textline-parsed input-path) ["?groupid"]) ?groupid)
+       (o/count ?count)))
+
+
+;; (?<- (stdout) (textline-parsed input-path) input-path)              ;; print all rows
+;; (query-uniq-group-ids (stdout) input-path))) ;; print rows which have a unique identity
