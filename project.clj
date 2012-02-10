a43fca56d929ec0113304e5ba7b9d20c6d8a30be
Matt Stump <mstump@sourceninja.com>
2012-02-10 08:12:53 -0800

    updated to the newest snapshot version of cascalog 1.8.6

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject test "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cascalog "1.8.5"]
+                 [cascalog "1.8.6-SNAPSHOT"]
                  [cascading.pingles/cascading.cassandra "0.0.1-SNAPSHOT"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.204.0"]]
   :repositories {"conjars" "http://conjars.org/repo"}
